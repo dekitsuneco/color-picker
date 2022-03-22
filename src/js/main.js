746f90f3e35bc4ccf42067e011843780372aadf3
Akira <47333988+dekitsuneco@users.noreply.github.com>
2022-03-23 02:16:37 +0500

    Add styles for color widget

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+/**
+ * @type {Object}
+ */
+const colorjoe = {
+  rgb: (dom) => this,
+};
+/**
+ * @type {Object}
+ */
+const colorWidget = colorjoe.rgb(
+  document.querySelector('.color-widget__picker'),
+);
+
+colorWidget.show();
